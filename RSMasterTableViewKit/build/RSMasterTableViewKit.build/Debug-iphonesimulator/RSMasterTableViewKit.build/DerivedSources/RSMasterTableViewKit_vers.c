57e9a6ba74f553141c1b61af7f4b20d1cce10e64
Rushi Sangani <rushisangani@gmail.com>
2018-09-04 15:45:23 +0530

    - Build folder removed.

--- RSMasterTableViewKit/build/RSMasterTableViewKit.build/Debug-iphonesimulator/RSMasterTableViewKit.build/DerivedSources/RSMasterTableViewKit_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char RSMasterTableViewKitVersionString[];
- extern const double RSMasterTableViewKitVersionNumber;
-
- const unsigned char RSMasterTableViewKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:RSMasterTableViewKit  PROJECT:RSMasterTableViewKit-1" "\n";
- const double RSMasterTableViewKitVersionNumber __attribute__ ((used)) = (double)1.;
--- RSMasterTableViewKit/build/RSMasterTableViewKit.build/Debug-iphonesimulator/RSMasterTableViewKit.build/Objects-normal/x86_64/RSMasterTableViewKit_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Rushi\ Sangani/GitHub/RSMasterTableViewKit/RSMasterTableViewKit/build/RSMasterTableViewKit.build/Debug-iphonesimulator/RSMasterTableViewKit.build/DerivedSources/RSMasterTableViewKit_vers.c
